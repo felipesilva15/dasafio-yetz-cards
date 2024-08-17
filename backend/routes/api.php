b7b114589aa5b2a3be5fc3ead4e02f5d4ffb9dd2
Felipe Silva <felipe.allware@gmail.com>
2024-08-16 23:01:49 -0300

    style: Limpando código do arquivos de rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 // Games
 Route::apiResource('/games', GameController::class);
 

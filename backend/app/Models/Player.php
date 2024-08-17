207f0b3f1ee83a509eddd1ca1e036e0fd5bb9a38
Felipe Silva <felipe.allware@gmail.com>
2024-08-16 22:26:08 -0300

    feat: Criando Model de Player

---
+++ backend/app/Models/Player.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'level',
+        'goalkeeper'
+    ];
+
+    protected $cast = [
+        'goalkeeper' => 'boolean'
+    ];
+}

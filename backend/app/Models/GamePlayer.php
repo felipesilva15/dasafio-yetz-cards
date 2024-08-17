47594f9ecb12cd4b4f1d54a0376634d8cfc7579e
Felipe Silva <felipe.allware@gmail.com>
2024-08-16 22:26:45 -0300

    feat: Criando Model de GamePlayer

---
+++ backend/app/Models/GamePlayer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class GamePlayer extends Pivot
+{
+    protected $table = 'game_player';
+
+    protected $fillable = [
+        'game_id', 
+        'player_id', 
+        'team_id',
+        'confirmed'
+    ];
+
+    protected $cast = [
+        'confirmed' => 'boolean'
+    ];
+
+    public function team(): BelongsTo {
+        return $this->belongsTo(Team::class);
+    }
+}

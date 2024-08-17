9ce17ea9b64300e146d00e658b8b5df14b8f0d03
Felipe Silva <felipe.allware@gmail.com>
2024-08-16 22:49:13 -0300

    feat: Criando controller base da aplicação

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\NotFoundHttpException;
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    protected $model;
+
+    public function index() {
+        $query = $this->model::query();
+        $query->orderBy('id', 'desc');
+
+        $data = $query->get();
+
+        return response()->json($data, 200);
+    }
+
+    public function show($id) {
+        $data = $this->model::find($id);
+        
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request) {
+        if (method_exists($this->model, 'rules')){
+            $request->validate($this->model::rules());
+        }
+        
+        $data = $this->model::create($request->all());
+
+        return response()->json($data, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $data = $this->model::find($id);
+
+        if (!$data) {
+            throw new NotFoundHttpException();
+        }
+
+        if (method_exists($this->model, 'rules')){
+            $request->validate($this->model::rules());
+        }
+            
+        $data->update($request->all());
+
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id) {
+        $data = $this->model::find($id);
+
+        if (!$data) {
+            throw new NotFoundHttpException;
+        }
+
+        $data->delete();
+
+        return response()->json(['message' => 'Registro deletado com sucesso!'], 200);
+    }
 }
